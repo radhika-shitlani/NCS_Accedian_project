33bad0afbfedbb210ff30219cce2df5a475a8588
Dipankar <dipcom02323@gmail.com>
2020-08-06 12:16:07 +0000

    spirent Code added

--- csit/libraries/CCA.py
+++ csit/libraries/CCA.py
@@ -12,7 +12,7 @@ import csv
 import textfsm
 from service import Service
 import yaml
-#from Class_Based_Spirent_Code_Generation import Spirent_L2_Traffic_Gen,Get_Spirent_Config,Create_Spirent_L2_Gen
+from Class_Based_Spirent_Code_Generation import Spirent_L2_Traffic_Gen,Get_Spirent_Config,Create_Spirent_L2_Gen
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -28,19 +28,23 @@ def onnet_CCM_Y1564():
     test_result = {}
     test_result['ccm_status'] = my_config.Validate_ccm()
     test_result['Loop_test'] = my_config.Y1564_test()
+  
+    Spirent_InputParam = {
+        'Frame_Size':9100, 
+        'Rate_Mbps': 400,
+        'vlan_id' : 2301,
+        'vlan_user_priority': 1
+        }
+    Spirent_L2_Gen = Create_Spirent_L2_Gen()
+    Spirent_L2_Gen.Port_Init()
+    print("Just Before Stream Creation-1")
+    StreamHandle = Spirent_L2_Gen.Stream_Config_Creation_Single_Tagged_VLAN_Mbps(0,1,**Spirent_InputParam)
+    #Spirent_L2_Gen.Generate_Stream_Traffic(StreamHandle)
+    print("Just Before Stream Creation-2")
+    StreamHandle = Spirent_L2_Gen.Stream_Config_Creation_Single_Tagged_VLAN_Mbps(1,0,**Spirent_InputParam)
+    #Spirent_L2_Gen.Generate_Stream_Traffic(StreamHandle)
+    Spirent_L2_Gen.Generate_Traffic()
+    Spirent_L2_Gen.Traffic_Collection()
+    test_result['spirent_test'] = Spirent_L2_Gen.Validate_Traffic_Result()
     my_config.delete_config()
     return test_result
-
-    # Spirent_L2_Gen = Create_Spirent_L2_Gen()
-    # Spirent_L2_Gen.Port_Init()
-    # print("Just Before Stream Creation-1")
-    # StreamHandle = Spirent_L2_Gen.Stream_Config_Creation_Without_VLAN_Mbps(0,1)
-    # #Spirent_L2_Gen.Generate_Stream_Traffic(StreamHandle)
-    # print("Just Before Stream Creation-2")
-    # StreamHandle = Spirent_L2_Gen.Stream_Config_Creation_Without_VLAN_Mbps(1,0)
-    # #Spirent_L2_Gen.Generate_Stream_Traffic(StreamHandle)
-    # Spirent_L2_Gen.Generate_Traffic()
-    # Spirent_L2_Gen.Traffic_Collection()
-    # Status = Spirent_L2_Gen.Validate_Traffic_Result()
-
-    #Spirent_InputParam = {'Ether_Speed':'ether1000', 'Stream_Name':'Spirent_Class_Test', 'Frame_Size':2000, 'Rate_Mbps': 100}
