b9f0df7b425091e252f0e75019f4cc5e2c8bed70
Dipankar <dipcom02323@gmail.com>
2020-08-16 15:06:39 +0000

    robot

--- csit/libraries/CCA.py
+++ csit/libraries/CCA.py
@@ -15,7 +15,7 @@ import textfsm
 from service import Service
 import yaml
 import ast
-from Class_Based_Spirent_Code_Generation import Spirent_L2_Traffic_Gen,Get_Spirent_Config,Create_Spirent_L2_Gen
+#from Class_Based_Spirent_Code_Generation import Spirent_L2_Traffic_Gen,Get_Spirent_Config,Create_Spirent_L2_Gen
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 result = {}
@@ -37,15 +37,15 @@ def onnet_CCA(A,B):
     my_config.parse_accedian()
     my_config.Command_Creation()
     my_config.push_config()
-    time.sleep(10)
+    time.sleep(20)
     test_result = {}
     test_result['ccm_status'] = my_config.Validate_ccm()
     #test_result['Loop_test'] = my_config.Y1564_test()
-    input_dict = {}
-    input_dict = my_config.create_spirent_input_dict()
-    pprint(input_dict)
-    Spirent_L2_Gen = Create_Spirent_L2_Gen()
-    Spirent_L2_Gen.Port_Init()
+    # input_dict = {}
+    # input_dict = my_config.create_spirent_input_dict()
+    # pprint(input_dict)
+    # Spirent_L2_Gen = Create_Spirent_L2_Gen()
+    # Spirent_L2_Gen.Port_Init()
     # for tr in ['UC','BC','MC']:
     #     if A == 'Y':
     #         StreamHandle1 = Spirent_L2_Gen.Stream_Config_Creation_Dual_Tagged_VLAN_dot1ad_Mbps(0,1,**input_dict['Spirent_2TAG_AZ'][tr])
@@ -66,27 +66,27 @@ def onnet_CCA(A,B):
     #     test_result['Spirent_{}_traffic'.format(tr)] = Spirent_L2_Gen.Validate_Traffic_Result2()
     #     Spirent_L2_Gen.delete_streams_clear_counters()
 
-    for mt_vt in ['MT','VT']:
-        print("**** {} traffic is going to run".format(mt_vt))
-        if A == 'P':
-            if mt_vt == 'MT':
-                StreamHandleMT = Spirent_L2_Gen.Spirent_MAC_Transperancy_Traffic_Testing_For_P2P_Service(0,1,**input_dict['Spirent_0TAG_AZ']['UC'])
-            else:
-                StreamHandleVT = Spirent_L2_Gen.Spirent_VLAN_Transperancy_Traffic_Testing_For_P2P_Service(0,1,**input_dict['Spirent_1TAG_AZ']['UC'])
-        if B == 'P':
-            if mt_vt == 'MT':
-                StreamHandleMT = Spirent_L2_Gen.Spirent_MAC_Transperancy_Traffic_Testing_For_P2P_Service(1,0,**input_dict['Spirent_0TAG_ZA']['UC'])
-            else:
-                StreamHandleVT = Spirent_L2_Gen.Spirent_VLAN_Transperancy_Traffic_Testing_For_P2P_Service(1,0,**input_dict['Spirent_1TAG_ZA']['UC'])
-        Spirent_L2_Gen.Generate_Traffic()
-        Spirent_L2_Gen.Traffic_Collection()
-        test_result['Spirent_{}_traffic'.format(mt_vt)] = Spirent_L2_Gen.Validate_Traffic_Result2()
-        Spirent_L2_Gen.delete_streams_clear_counters()    
+    # for mt_vt in ['MT','VT']:
+    #     print("**** {} traffic is going to run".format(mt_vt))
+    #     if A == 'P':
+    #         if mt_vt == 'MT':
+    #             StreamHandleMT = Spirent_L2_Gen.Spirent_MAC_Transperancy_Traffic_Testing_For_P2P_Service(0,1,**input_dict['Spirent_0TAG_AZ']['UC'])
+    #         else:
+    #             StreamHandleVT = Spirent_L2_Gen.Spirent_VLAN_Transperancy_Traffic_Testing_For_P2P_Service(0,1,**input_dict['Spirent_1TAG_AZ']['UC'])
+    #     if B == 'P':
+    #         if mt_vt == 'MT':
+    #             StreamHandleMT = Spirent_L2_Gen.Spirent_MAC_Transperancy_Traffic_Testing_For_P2P_Service(1,0,**input_dict['Spirent_0TAG_ZA']['UC'])
+    #         else:
+    #             StreamHandleVT = Spirent_L2_Gen.Spirent_VLAN_Transperancy_Traffic_Testing_For_P2P_Service(1,0,**input_dict['Spirent_1TAG_ZA']['UC'])
+    #     Spirent_L2_Gen.Generate_Traffic()
+    #     Spirent_L2_Gen.Traffic_Collection()
+    #     test_result['Spirent_{}_traffic'.format(mt_vt)] = Spirent_L2_Gen.Validate_Traffic_Result2()
+    #     Spirent_L2_Gen.delete_streams_clear_counters()    
 
-    Spirent_L2_Gen.Clean_Up_Spirent()
-    test_result['CFM_Stats_Acc'] = my_config.mep_statistic_accedian()
-    test_result['CFM_Stats_cisco'] = my_config.mep_statistic_cisco()
-    my_config.check_QOS_counters_config()
+    # Spirent_L2_Gen.Clean_Up_Spirent()
+    # test_result['CFM_Stats_Acc'] = my_config.mep_statistic_accedian()
+    # test_result['CFM_Stats_cisco'] = my_config.mep_statistic_cisco()
+    # my_config.check_QOS_counters_config()
     my_config.parse_accedian()
     my_config.Command_Creation()
     my_config.delete_config()
